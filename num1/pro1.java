6c19cf47d66d7265ad40da613cb3658f59d82852
kbh626 <kobh0626@naver.com>
2022-10-08 19:45:34 +0900

    first commit

---
+++ num1/pro1.java
@@ -0,0 +1,13 @@
+/**
+ * pro1
+ */
+public class pro1 {
+
+    public static void main(String[] args) {
+        int num;
+        int k;
+        for(int i=0;i<num.length;i++){
+            
+        }
+    }
+}
